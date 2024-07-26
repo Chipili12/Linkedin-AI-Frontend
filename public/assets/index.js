213dd4021d385f98f4bc3b6240c7e3c8abf5e607
Chipili12 <santibur@live.com>
2024-07-26 18:26:50 -0300

    case sensitive

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,6 +1,6 @@
 import dark_favicon from "./dark_favicon.ico";
 import light_favicon from "./light_favicon.ico";
-import logo from "./Logo.png";
+import logo from "./logo.png";
 import menu_icon from "./menu_icon.svg";
 import ai_cellphone from './ai_cellphone.jpg';
 import phone from './phone.png';
