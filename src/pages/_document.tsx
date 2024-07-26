9df50db962e4bf8e296bfc8fdc5de10170776ffa
Chipili12 <santibur@live.com>
2024-07-26 18:34:13 -0300

    formating

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,25 @@
-'use client'
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+"use client";
+import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="icon" href="/assets/dark_favicon.ico" media="(prefers-color-scheme: dark)"/>
-      <link rel="icon" media="(prefers-color-scheme: light)" href="/assets/light_favicon.ico"/>
+        <link
+          rel="icon"
+          href="/assets/dark_favicon.ico"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          media="(prefers-color-scheme: light)"
+          href="/assets/light_favicon.ico"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
